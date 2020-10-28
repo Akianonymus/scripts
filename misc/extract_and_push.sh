5b6553992235547741fecf3b132c237a22c1d223
Akianonymus <anonymus.aki@gmail.com>
2020-10-28 11:31:12 +0530

    misc/extract_and_push.sh: Cleanup and some improvements
    remove some useless checks
    
    use bash substition instead of tr and cut where possible

--- misc/extract_and_push.sh
+++ misc/extract_and_push.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl --fail --silent --location https://git.rip > /dev/null || exit 1
+curl --fail --silent --location https://git.rip -o /dev/null || exit 1
 
 [[ -z ${API_KEY} ]] && echo "API_KEY not defined, exiting!" && exit 1
 
@@ -90,42 +90,38 @@ if [[ ! -f ${FILE} ]]; then
     fi
 fi
 
-PARTITIONS="system vendor cust odm oem factory product modem xrom systemex system_ext system_other oppo_product opproduct reserve india my_preload my_odm my_stock my_operator my_country my_product my_company my_engineering my_heytap"
+EXTERNAL_TOOLS=(
+    https://github.com/PabloCastellano/extract-dtb
+    https://github.com/AndroidDumps/Firmware_extractor
+    https://github.com/xiaolu/mkbootimg_tools
+    https://github.com/marin-m/vmlinux-to-elf
+)
 
-if [[ ! -d "${HOME}/extract-dtb" ]]; then
-    git clone -q https://github.com/PabloCastellano/extract-dtb ~/extract-dtb
-else
-    git -C ~/extract-dtb pull
-fi
+for tool_url in "${EXTERNAL_TOOLS[@]}"; do
+    tool_path="${HOME}/${tool_url##*/}"
+    if ! [[ -d "${tool_path}" ]]; then
+        git clone -q "${tool_url}" "${tool_path}"
+    else
+        git -C "${tool_path}" pull
+    fi
+done
 
-if [[ ! -d "${HOME}/Firmware_extractor" ]]; then
-    git clone -q https://github.com/AndroidDumps/Firmware_extractor ~/Firmware_extractor
-else
-    git -C ~/Firmware_extractor pull
-fi
-
-if [[ ! -d "${HOME}/mkbootimg_tools" ]]; then
-    git clone -q https://github.com/xiaolu/mkbootimg_tools ~/mkbootimg_tools
-else
-    git -C ~/mkbootimg_tools pull
-fi
-
-if [[ ! -d "${HOME}/vmlinux-to-elf" ]]; then
-    git clone -q https://github.com/marin-m/vmlinux-to-elf ~/vmlinux-to-elf
-else
-    git -C ~/vmlinux-to-elf pull
-fi
-
-bash ~/Firmware_extractor/extractor.sh "${FILE}" "${PWD}" || {
+bash "${HOME}"/Firmware_extractor/extractor.sh "${FILE}" "${PWD}" || {
     sendTG edit "${MESSAGE_ID}" "${MESSAGE}"$'\n'"Extraction failed!" > /dev/null
     exit 1
 }
 
 rm -fv "$FILE"
 
+PARTITIONS=(system systemex system_ext system_other
+    vendor cust odm oem factory product modem
+    xrom reserve india oppo_product opproduct
+    my_preload my_odm my_stock my_operator my_country my_product my_company my_engineering my_heytap
+)
+
 # Extract the images
-for p in $PARTITIONS; do
-    if [ -f "$p.img" ]; then
+for p in "${PARTITIONS[@]}"; do
+    if [[ -f $p.img ]]; then
         mkdir "$p" || rm -rf "${p:?}"/*
         7z x "$p".img -y -o"$p"/ || {
             sudo mount -o loop "$p".img "$p"
@@ -165,21 +161,23 @@ fi
 # Extract bootimage and dtbo
 if [[ -f "boot.img" ]]; then
     mkdir -v bootdts
-    ~/mkbootimg_tools/mkboot ./boot.img ./bootimg > /dev/null
-    python3 ~/extract-dtb/extract-dtb.py ./boot.img -o ./bootimg > /dev/null
+    "${HOME}"/mkbootimg_tools/mkboot ./boot.img ./bootimg > /dev/null
+    python3 "${HOME}"/extract-dtb/extract-dtb.py ./boot.img -o ./bootimg > /dev/null
     find bootimg/ -name '*.dtb' -type f -exec dtc -I dtb -O dts {} -o bootdts/"$(echo {} | sed 's/\.dtb/.dts/')" \; > /dev/null 2>&1
+
     # Extract ikconfig
-    if [[ "$(command -v extract-ikconfig)" ]]; then
+    command -v extract-ikconfig > /dev/null &&
         extract-ikconfig boot.img > ikconfig
-    fi
+
     # Kallsyms
-    python3 ~/vmlinux-to-elf/vmlinux_to_elf/kallsyms_finder.py boot.img > kallsyms.txt
+    python3 "${HOME}"/vmlinux-to-elf/vmlinux_to_elf/kallsyms_finder.py boot.img > kallsyms.txt
+
     # ELF
-    python3 ~/vmlinux-to-elf/vmlinux_to_elf/main.py boot.img boot.elf
+    python3 "${HOME}"/vmlinux-to-elf/vmlinux_to_elf/main.py boot.img boot.elf
 fi
 if [[ -f "dtbo.img" ]]; then
     mkdir -v dtbodts
-    python3 ~/extract-dtb/extract-dtb.py ./dtbo.img -o ./dtbo > /dev/null
+    python3 "${HOME}"/extract-dtb/extract-dtb.py ./dtbo.img -o ./dtbo > /dev/null
     find dtbo/ -name '*.dtb' -type f -exec dtc -I dtb -O dts {} -o dtbodts/"$(echo {} | sed 's/\.dtb/.dts/')" \; > /dev/null 2>&1
 fi
 
@@ -197,7 +195,7 @@ fi
 # board-info.txt
 find ./modem -type f -exec strings {} \; | grep "QC_IMAGE_VERSION_STRING=MPSS." | sed "s|QC_IMAGE_VERSION_STRING=MPSS.||g" | cut -c 4- | sed -e 's/^/require version-baseband=/' >> ./board-info.txt
 find ./tz* -type f -exec strings {} \; | grep "QC_IMAGE_VERSION_STRING" | sed "s|QC_IMAGE_VERSION_STRING|require version-trustzone|g" >> ./board-info.txt
-if [ -f ./vendor/build.prop ]; then
+if [[ -f ./vendor/build.prop ]]; then
     strings ./vendor/build.prop | grep "ro.vendor.build.date.utc" | sed "s|ro.vendor.build.date.utc|require version-vendor|g" >> ./board-info.txt
 fi
 sort -u -o ./board-info.txt ./board-info.txt
@@ -215,30 +213,37 @@ flavor=$(grep -oP "(?<=^ro.build.flavor=).*" -hs {system,system/system,vendor}/b
 [[ -z ${flavor} ]] && flavor=$(grep -oP "(?<=^ro.vendor.build.flavor=).*" -hs vendor/build*.prop)
 [[ -z ${flavor} ]] && flavor=$(grep -oP "(?<=^ro.system.build.flavor=).*" -hs {system,system/system}/build*.prop)
 [[ -z ${flavor} ]] && flavor=$(grep -oP "(?<=^ro.build.type=).*" -hs {system,system/system}/build*.prop)
+
 release=$(grep -oP "(?<=^ro.build.version.release=).*" -hs {system,system/system,vendor}/build*.prop)
 [[ -z ${release} ]] && release=$(grep -oP "(?<=^ro.vendor.build.version.release=).*" -hs vendor/build*.prop)
 [[ -z ${release} ]] && release=$(grep -oP "(?<=^ro.system.build.version.release=).*" -hs {system,system/system}/build*.prop)
+
 id=$(grep -oP "(?<=^ro.build.id=).*" -hs {system,system/system,vendor}/build*.prop)
 [[ -z ${id} ]] && id=$(grep -oP "(?<=^ro.vendor.build.id=).*" -hs vendor/build*.prop)
 [[ -z ${id} ]] && id=$(grep -oP "(?<=^ro.system.build.id=).*" -hs {system,system/system}/build*.prop)
+
 incremental=$(grep -oP "(?<=^ro.build.version.incremental=).*" -hs {system,system/system,vendor}/build*.prop)
 [[ -z ${incremental} ]] && incremental=$(grep -oP "(?<=^ro.vendor.build.version.incremental=).*" -hs vendor/build*.prop)
 [[ -z ${incremental} ]] && incremental=$(grep -oP "(?<=^ro.system.build.version.incremental=).*" -hs {system,system/system}/build*.prop)
 [[ -z ${incremental} ]] && incremental=$(grep -oP "(?<=^ro.build.version.incremental=).*" -hs my_product/build*.prop)
 [[ -z ${incremental} ]] && incremental=$(grep -oP "(?<=^ro.system.build.version.incremental=).*" -hs my_product/build*.prop)
 [[ -z ${incremental} ]] && incremental=$(grep -oP "(?<=^ro.vendor.build.version.incremental=).*" -hs my_product/build*.prop)
+
 tags=$(grep -oP "(?<=^ro.build.tags=).*" -hs {system,system/system,vendor}/build*.prop)
 [[ -z ${tags} ]] && tags=$(grep -oP "(?<=^ro.vendor.build.tags=).*" -hs vendor/build*.prop)
 [[ -z ${tags} ]] && tags=$(grep -oP "(?<=^ro.system.build.tags=).*" -hs {system,system/system}/build*.prop)
+
 platform=$(grep -oP "(?<=^ro.board.platform=).*" -hs {system,system/system,vendor}/build*.prop)
 [[ -z ${platform} ]] && platform=$(grep -oP "(?<=^ro.vendor.board.platform=).*" -hs vendor/build*.prop)
 [[ -z ${platform} ]] && platform=$(grep -oP rg"(?<=^ro.system.board.platform=).*" -hs {system,system/system}/build*.prop)
+
 manufacturer=$(grep -oP "(?<=^ro.product.manufacturer=).*" -hs {system,system/system,vendor}/build*.prop)
 [[ -z ${manufacturer} ]] && manufacturer=$(grep -oP "(?<=^ro.vendor.product.manufacturer=).*" -hs vendor/build*.prop)
 [[ -z ${manufacturer} ]] && manufacturer=$(grep -oP "(?<=^ro.system.product.manufacturer=).*" -hs {system,system/system}/build*.prop)
 [[ -z ${manufacturer} ]] && manufacturer=$(grep -oP "(?<=^ro.system.product.manufacturer=).*" -hs vendor/euclid/*/build.prop)
 [[ -z ${manufacturer} ]] && manufacturer=$(grep -oP "(?<=^ro.product.manufacturer=).*" -hs oppo_product/build*.prop)
 [[ -z ${manufacturer} ]] && manufacturer=$(grep -oP "(?<=^ro.product.manufacturer=).*" -hs my_product/build*.prop)
+
 fingerprint=$(grep -oP "(?<=^ro.vendor.build.fingerprint=).*" -hs vendor/build.prop)
 [[ -z ${fingerprint} ]] && fingerprint=$(grep -oP "(?<=^ro.build.fingerprint=).*" -hs {system,system/system}/build.prop)
 [[ -z ${fingerprint} ]] && fingerprint=$(grep -oP "(?<=^ro.build.fingerprint=).*" -hs {system,system/system}/build*.prop)
@@ -249,6 +254,7 @@ fingerprint=$(grep -oP "(?<=^ro.vendor.build.fingerprint=).*" -hs vendor/build.p
 [[ -z ${fingerprint} ]] && fingerprint=$(grep -oP "(?<=^ro.build.fingerprint=).*" -hs my_product/build.prop)
 [[ -z ${fingerprint} ]] && fingerprint=$(grep -oP "(?<=^ro.system.build.fingerprint=).*" -hs my_product/build.prop)
 [[ -z ${fingerprint} ]] && fingerprint=$(grep -oP "(?<=^ro.vendor.build.fingerprint=).*" -hs my_product/build.prop)
+
 brand=$(grep -oP "(?<=^ro.product.brand=).*" -hs {system,system/system,vendor}/build*.prop | head -1)
 [[ -z ${brand} ]] && brand=$(grep -oP "(?<=^ro.product.vendor.brand=).*" -hs vendor/build*.prop | head -1)
 [[ -z ${brand} ]] && brand=$(grep -oP "(?<=^ro.vendor.product.brand=).*" -hs vendor/build*.prop | head -1)
@@ -258,6 +264,7 @@ brand=$(grep -oP "(?<=^ro.product.brand=).*" -hs {system,system/system,vendor}/b
 [[ -z ${brand} ]] && brand=$(grep -oP "(?<=^ro.product.brand=).*" -hs oppo_product/build*.prop)
 [[ -z ${brand} ]] && brand=$(grep -oP "(?<=^ro.product.brand=).*" -hs my_product/build*.prop)
 [[ -z ${brand} ]] && brand=$(echo "$fingerprint" | cut -d / -f1)
+
 codename=$(grep -oP "(?<=^ro.product.device=).*" -hs {system,system/system,vendor}/build*.prop | head -1)
 [[ -z ${codename} ]] && codename=$(grep -oP "(?<=^ro.product.vendor.device=).*" -hs vendor/build*.prop | head -1)
 [[ -z ${codename} ]] && codename=$(grep -oP "(?<=^ro.vendor.product.device=).*" -hs vendor/build*.prop | head -1)
@@ -269,6 +276,7 @@ codename=$(grep -oP "(?<=^ro.product.device=).*" -hs {system,system/system,vendo
 [[ -z ${codename} ]] && codename=$(grep -oP "(?<=^ro.product.vendor.device=).*" -hs my_product/build*.prop)
 [[ -z ${codename} ]] && codename=$(grep -oP "(?<=^ro.build.fota.version=).*" -hs {system,system/system}/build*.prop | cut -d - -f1 | head -1)
 [[ -z ${codename} ]] && codename=$(echo "$fingerprint" | cut -d / -f3 | cut -d : -f1)
+
 description=$(grep -oP "(?<=^ro.build.description=).*" -hs {system,system/system}/build.prop)
 [[ -z ${description} ]] && description=$(grep -oP "(?<=^ro.build.description=).*" -hs {system,system/system}/build*.prop)
 [[ -z ${description} ]] && description=$(grep -oP "(?<=^ro.vendor.build.description=).*" -hs vendor/build.prop)
@@ -277,54 +285,60 @@ description=$(grep -oP "(?<=^ro.build.description=).*" -hs {system,system/system
 [[ -z ${description} ]] && description=$(grep -oP "(?<=^ro.product.build.description=).*" -hs product/build*.prop)
 [[ -z ${description} ]] && description=$(grep -oP "(?<=^ro.system.build.description=).*" -hs {system,system/system}/build*.prop)
 [[ -z ${description} ]] && description="$flavor $release $id $incremental $tags"
-branch=$(echo "$description" | tr ' ' '-')
-repo_subgroup=$(echo "$brand" | tr '[:upper:]' '[:lower:]')
-[[ -z $repo_subgroup ]] && repo_subgroup=$(echo "$manufacturer" | tr '[:upper:]' '[:lower:]')
-repo_name=$(echo "$codename" | tr '[:upper:]' '[:lower:]')
-repo="$repo_subgroup/$repo_name"
-platform=$(echo "$platform" | tr '[:upper:]' '[:lower:]' | tr -dc '[:print:]' | tr '_' '-' | cut -c 1-35)
-top_codename=$(echo "$codename" | tr '[:upper:]' '[:lower:]' | tr -dc '[:print:]' | tr '_' '-' | cut -c 1-35)
-manufacturer=$(echo "$manufacturer" | tr '[:upper:]' '[:lower:]' | tr -dc '[:print:]' | tr '_' '-' | cut -c 1-35)
 
-printf "\nflavor: %s\nrelease: %s\nid: %s\nincremental: %s\ntags: %s\nfingerprint: %s\nbrand: %s\ncodename: %s\ndescription: %s\nbranch: %s\nrepo: %s\nmanufacturer: %s\nplatform: %s\ntop_codename: %s\n" "$flavor" "$release" "$id" "$incremental" "$tags" "$fingerprint" "$brand" "$codename" "$description" "$branch" "$repo" "$manufacturer" "$platform" "$top_codename"
+branch="${description// /_}"
+repo_subgroup="${brand,,}"
+[[ -z $repo_subgroup ]] && repo_subgroup="${manufacturer,,}"
+repo_name="${codename,,}"
+repo="$repo_subgroup/$repo_name"
+platform="$(: "${platform,,}" && : "${_//_/-/}" && printf "%s\n" "${_:0:135}")"
+top_codename="$(: "${codename,,}" && : "${_//_/-/}" && printf "%s\n" "${_:0:135}")"
+manufacturer="$(: "${manufacturer,,}" && : "${_//_/-/}" && printf "%s\n" "${_:0:135}")"
+
+printf "\n%s\n\n" \
+    "flavor: $flavor
+    release: $release
+    id: $id
+    incremental: $incremental
+    tags: $tags
+    fingerprint: $fingerprint
+    brand: $brand
+    codename: $codename
+    description: $description
+    branch: $branch
+    repo: $repo
+    manufacturer: $manufacturer
+    platform: $platform
+    top_codename: $top_codename"
 
 # Check whether the subgroup exists or not
-if ! curl -s -H "Authorization: Bearer $DUMPER_TOKEN" "https://git.rip/api/v4/groups/$ORG%2f$repo_subgroup" -s --fail > x; then
-    if ! curl -H "Authorization: Bearer $DUMPER_TOKEN" "https://git.rip/api/v4/groups" -X POST -F name="${repo_subgroup^}" -F parent_id=562 -F path="${repo_subgroup}" --silent --fail > x; then
+if ! group_id_json="$(curl -s -H "Authorization: Bearer $DUMPER_TOKEN" "https://git.rip/api/v4/groups/$ORG%2f$repo_subgroup" -s --fail)"; then
+    if ! group_id_json="$(curl -H "Authorization: Bearer $DUMPER_TOKEN" "https://git.rip/api/v4/groups" -X POST -F name="${repo_subgroup^}" -F parent_id=562 -F path="${repo_subgroup}" --silent --fail)"; then
         sendTG edit "${MESSAGE_ID}" "${MESSAGE}"$'\n'"Creating subgroup for $repo_subgroup failed!" > /dev/null
         exit 1
     fi
 fi
-group_id="$(jq -r '.id' x)"
-rm -f x
 
-[[ -z $group_id ]] && {
+if ! group_id="$(jq '.id' -e <<< "${group_id_json}")"; then
     sendTG edit "${MESSAGE_ID}" "${MESSAGE}"$'\n'"Unable to get gitlab group id!" > /dev/null
     exit 1
-}
+fi
 
 # Create the repo if it doesn't exist
-curl --silent -H "Authorization: bearer ${DUMPER_TOKEN}" "https://git.rip/api/v4/projects/$ORG%2f$repo_subgroup%2f$repo_name" > x
-message="$(jq -r .message x)"
-project_id="$(jq .id x)"
-rm -f x
-if [[ $message == "404 Project Not Found" ]]; then
-    curl --silent -H "Authorization: bearer ${DUMPER_TOKEN}" "https://git.rip/api/v4/projects" -X POST -F namespace_id="$group_id" -F name="$repo_name" -F visibility=public > x
-    project_id="$(jq .id x)"
-    rm -f x
-    if [[ -z $project_id ]]; then
+project_id_json="$(curl --silent -H "Authorization: bearer ${DUMPER_TOKEN}" "https://git.rip/api/v4/projects/$ORG%2f$repo_subgroup%2f$repo_name")"
+if ! project_id="$(jq .id -e <<< "${project_id_json}")"; then
+    project_id_json="$(curl --silent -H "Authorization: bearer ${DUMPER_TOKEN}" "https://git.rip/api/v4/projects" -X POST -F namespace_id="$group_id" -F name="$repo_name" -F visibility=public)"
+    if project_id="$(jq .id -e <<< "${project_id_json}")"; then
         sendTG edit "${MESSAGE_ID}" "${MESSAGE}"$'\n'"Could not get project id" > /dev/null
         exit 1
     fi
 fi
 
-curl --silent -H "Authorization: bearer ${DUMPER_TOKEN}" "https://git.rip/api/v4/projects/$project_id/repository/branches/$branch" > x
-[[ "$(jq -r '.name' x)" == "$branch" ]] && {
+branch_json="$(curl --silent -H "Authorization: bearer ${DUMPER_TOKEN}" "https://git.rip/api/v4/projects/$project_id/repository/branches/$branch")"
+[[ "$(jq '.name' -e <<< "${branch_json}")" == "$branch" ]] && {
     sendTG edit "${MESSAGE_ID}" "${MESSAGE}"$'\n'"$branch already exists in <a href=\"https://git.rip/dumps/$repo\">$repo</a>!" > /dev/null
-    rm -f x
     exit 1
 }
-rm -f x
 
 # Add, commit, and push after filtering out certain files
 git init
@@ -351,20 +365,13 @@ sendTG edit "${MESSAGE_ID}" "${MESSAGE}"$'\n'"Pushed <a href=\"https://git.rip/$
 commit_head=$(git rev-parse HEAD)
 commit_link="https://git.rip/$ORG/$repo/commit/$commit_head"
 echo -e "Sending telegram notification"
-(
-    printf "<b>Brand: %s</b>" "$brand"
-    printf "\n<b>Device: %s</b>" "$codename"
-    printf "\n<b>Version:</b> %s" "$release"
-    printf "\n<b>Fingerprint:</b> %s" "$fingerprint"
-    printf "\n<b>Git link:</b>"
-    printf "\n<a href=\"%s\">Commit</a>" "$commit_link"
-    printf "\n<a href=\"https://git.rip/%s/%s/tree/%s/\">$codename</a>" "$ORG" "$repo" "$branch"
-) >> tg.html
-
-TEXT=$(cat tg.html)
+TEXT="<b>Brand: $brand</b>
+<b>Device: $codename</b>
+<b>Version:</b> $release
+<b>Fingerprint:</b> $fingerprint
+<b>Git link:</b>
+<a href=\"$commit_link\">Commit</a>
+<a href=\"https://git.rip/$ORG/$repo/tree/$branch/\">$codename</a>"
 
 # Send message to Telegram channel
 curl -s "https://api.telegram.org/bot${API_KEY}/sendmessage" --data "text=${TEXT}&chat_id=@android_dumps&parse_mode=HTML&disable_web_page_preview=True" > /dev/null
-
-# Delete file after sending message
-rm -fv tg.html
